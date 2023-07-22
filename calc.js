d8240cc282879b5a96f90a8bb120c2d9ff7b6dcc
Muni Neelam <112398733+Rohit-muni45@users.noreply.github.com>
2023-07-22 08:45:06 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+  }
+
+  function calculate() {
+    var display = document.getElementById("display");
+    var result = eval(display.value);
+    display.value = result;
+  }
+
+  function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
+
+  function log(){
+    var display = document.getElementById("display");
+    var int= parseInt(display.value,10);
+    var result=Math.sqrt(int);
+    display.value=result;
+  }
